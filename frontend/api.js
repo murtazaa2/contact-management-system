1edf8b7233359c2e22af7bb530ba2bb88ac0180e
murtazaa2 <murtazamoiz23@gmail.com>
2026-06-12 03:39:27 +0500

    Update API URL for production

--- frontend/api.js
+++ frontend/api.js
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: "http://localhost:8080"
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://contact-management-system-production-ddfe.up.railway.app"
 });
 
 export default api;
